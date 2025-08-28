a312f9cbc427c0db8c08f05b04a1d3aa026cf546
Deniz Mert Dilaverler <dmertdilaverler@gmail.com>
2025-08-28 23:46:01 +0200

    Embed struct example

--- example/main.go
+++ example/main.go
@@ -5,11 +5,23 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 
-	pngembed "github.com/sabhiram/png-embed"
+	pngembed "github.com/deniz-dilaverler/png-embed"
 )
+
+type InnerStruct struct {
+	InnerBool bool   `json:"inner_bool"`
+	InnerStr  string `json:"inner_str"`
+	InnerInt  int    `json:"inner_int"`
+}
+
+type SampleStruct struct {
+	StrVal    string      `json:"str_val"`
+	IntVal    int         `json:"int_val"`
+	BoolVal   bool        `json:"bool_val"`
+	StructVal InnerStruct `json:"struct_val"`
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -23,22 +35,43 @@ var (
 ////////////////////////////////////////////////////////////////////////////////
 
 func main() {
-	data, err := pngembed.EmbedFile(inputFile, key, value)
-	if err == nil {
-		err = ioutil.WriteFile(outputFile, data, 777)
+	s := SampleStruct{
+		StrVal:  "hello",
+		IntVal:  42,
+		BoolVal: true,
+		StructVal: InnerStruct{
+			InnerBool: false,
+			InnerStr:  "world",
+			InnerInt:  7,
+		},
 	}
 
+	input, err := os.ReadFile(inputFile)
 	if err != nil {
-		fmt.Printf("Fatal error: %s\n", err.Error())
-		os.Exit(1)
+		panic(err)
+	}
+	data, err := pngembed.EmbedITXT(input, key, s)
+	if err == nil {
+		file, err := os.Create(outputFile)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		// Write binary data
+		_, err = file.Write(data)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("File '%s' was successfully embedded and saved as '%s' \n", inputFile, outputFile)
 	}
 }
 
 func init() {
-	flag.StringVar(&inputFile, "input", "", "input file name for the png")
+	flag.StringVar(&inputFile, "input", "image.png", "input file name for the png")
 	flag.StringVar(&outputFile, "output", "out.png", "output file name for the png")
 	flag.StringVar(&key, "key", "TEST_KEY", "key name for the data to inject")
-	flag.StringVar(&value, "value", "TEST_VALUE", "sample value to inject for key")
+	// flag.StringVar(&value, "value", "TEST_VALUE", "sample value to inject for key")
 
 	flag.Parse()
 	if len(inputFile) == 0 {
